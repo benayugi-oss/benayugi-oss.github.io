b0f503a32be38e251ef6cd3072e8905ec7ad1306
benayugi-oss <benayugi@hunschool.org>
2026-04-16 09:37:53 -0400

    Add alert button functionality in script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
+// 1. Select the button using its ID
+const alertButton = document.getElementById('myAlertButton');
 
+// 2. Add an event listener to run code when clicked
+alertButton.addEventListener('click', function() {
+    // 3. The alert action
+    alert('The external JavaScript file is working!');
+});
